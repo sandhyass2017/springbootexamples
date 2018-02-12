cdb2a1f241a7ab1e45b59538deecd50284d852a2
668450 <668450@PC387907.cts.com>
2018-02-12 14:11:44 +0530

    simple spring boot project using spring initialzer

---
+++ HelloWorldExample/src/main/java/com/example/hello/world/HelloWorldExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.hello.world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldExampleApplication.class, args);
+	}
+}
