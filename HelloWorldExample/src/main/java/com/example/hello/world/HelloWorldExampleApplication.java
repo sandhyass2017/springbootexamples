c90d7436923be6cc30d630ab0e79a1917e6970db
668450 <668450@PC387907.cts.com>
2018-02-13 16:48:22 +0530

    updated pom.xml and main class for Wildfly

--- HelloWorldExample/src/main/java/com/example/hello/world/HelloWorldExampleApplication.java
+++ HelloWorldExample/src/main/java/com/example/hello/world/HelloWorldExampleApplication.java
@@ -2,9 +2,16 @@ package com.example.hello.world;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HelloWorldExampleApplication {
+public class HelloWorldExampleApplication extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HelloWorldExampleApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldExampleApplication.class, args);
